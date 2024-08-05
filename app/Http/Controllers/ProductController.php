994a8195bd13b004c05404dedb8b4636831950b9
josephkristanto10 <josephkristanto10@gmail.com>
2024-08-05 20:57:34 +0700

    update 5 agustus 2024

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,7 +18,7 @@ class ProductController extends Controller
     public function index()
     {
         $category = Category::where('status_category', '=','1')->latest()->get();
-        $myproduct = Product::join("category",'category.id','=','product.id_category')->leftJoin("variant_product", "variant_product.id_product", '=',"product.id")->where('status_product', '=','1')->latest()->select("product.*", "category.name as category_name", "variant_product.variant_name", "variant_product.variant_images")->paginate(12);
+        $myproduct = Product::join("category",'category.id','=','product.id_category')->leftJoin("variant_product", "variant_product.id_product", '=',"product.id")->where('status_product', '=','1')->select("product.*", "category.name as category_name", "variant_product.variant_name", "variant_product.variant_images")->paginate(18);
         $array_product = [];
         $array_id = [];
         foreach($myproduct as $lp){
@@ -73,25 +73,25 @@ class ProductController extends Controller
         {
             if($idcategory == 0){
                 if($kata_kunci == ""){
-                    $myproduct =  Product::join("category",'category.id','=','product.id_category')->leftJoin("variant_product", "variant_product.id_product", '=',"product.id")->where('status_product', '=','1')->select("product.*", "category.name as category_name", "variant_product.variant_name", "variant_product.variant_images")->paginate(12);   
+                    $myproduct =  Product::join("category",'category.id','=','product.id_category')->leftJoin("variant_product", "variant_product.id_product", '=',"product.id")->where('status_product', '=','1')->select("product.*", "category.name as category_name", "variant_product.variant_name", "variant_product.variant_images")->paginate(18);   
                 }
                 else{
-                    $myproduct =  Product::join("category",'category.id','=','product.id_category')->leftJoin("variant_product", "variant_product.id_product", '=',"product.id")->where('status_product', '=','1')->where("product.code", 'like', '%'.$kata_kunci.'%')->orWhere("product.name", 'like', '%'.$kata_kunci.'%')->orWhere("category.name",'like', '%'.$kata_kunci.'%')->select("product.*", "category.name as category_name", "variant_product.variant_name", "variant_product.variant_images")->paginate(12);   
+                    $myproduct =  Product::join("category",'category.id','=','product.id_category')->leftJoin("variant_product", "variant_product.id_product", '=',"product.id")->where('status_product', '=','1')->where("product.code", 'like', '%'.$kata_kunci.'%')->orWhere("product.name", 'like', '%'.$kata_kunci.'%')->orWhere("category.name",'like', '%'.$kata_kunci.'%')->select("product.*", "category.name as category_name", "variant_product.variant_name", "variant_product.variant_images")->paginate(18);   
 
                   
                 }
 
             }
             else if($idcategory == -1){
-                $myproduct =  Product::join("category",'category.id','=','product.id_category')->leftJoin("variant_product", "variant_product.id_product", '=',"product.id")->where('status_product', '=','1')->latest('id')->select("product.*", "category.name as category_name", "variant_product.variant_name", "variant_product.variant_images")->paginate(12);   
+                $myproduct =  Product::join("category",'category.id','=','product.id_category')->leftJoin("variant_product", "variant_product.id_product", '=',"product.id")->where('status_product', '=','1')->latest('id')->select("product.*", "category.name as category_name", "variant_product.variant_name", "variant_product.variant_images")->paginate(18);   
 
             }
             else{
                 if($kata_kunci == ""){
-                    $myproduct =  Product::join("category",'category.id','=','product.id_category')->leftJoin("variant_product", "variant_product.id_product", '=',"product.id")->where('status_product', '=','1')->latest('id')->where('id_category','=',$idcategory)->select("product.*", "category.name as category_name", "variant_product.variant_name", "variant_product.variant_images")->paginate(12);
+                    $myproduct =  Product::join("category",'category.id','=','product.id_category')->leftJoin("variant_product", "variant_product.id_product", '=',"product.id")->where('status_product', '=','1')->latest('id')->where('id_category','=',$idcategory)->select("product.*", "category.name as category_name", "variant_product.variant_name", "variant_product.variant_images")->paginate(18);
                 }
                 else{
-                    $myproduct =  Product::join("category",'category.id','=','product.id_category')->leftJoin("variant_product", "variant_product.id_product", '=',"product.id")->where('status_product', '=','1')->where("product.code", 'like', '%'.$kata_kunci.'%')->orWhere("product.name", 'like', '%'.$kata_kunci.'%')->latest('id')->where('id_category','=',$idcategory)->orWhere("category.name",'like', '%'.$kata_kunci.'%')->select("product.*", "category.name as category_name", "variant_product.variant_name", "variant_product.variant_images")->paginate(12);
+                    $myproduct =  Product::join("category",'category.id','=','product.id_category')->leftJoin("variant_product", "variant_product.id_product", '=',"product.id")->where('status_product', '=','1')->where("product.code", 'like', '%'.$kata_kunci.'%')->orWhere("product.name", 'like', '%'.$kata_kunci.'%')->latest('id')->where('id_category','=',$idcategory)->orWhere("category.name",'like', '%'.$kata_kunci.'%')->select("product.*", "category.name as category_name", "variant_product.variant_name", "variant_product.variant_images")->paginate(18);
                 }
 
             }
